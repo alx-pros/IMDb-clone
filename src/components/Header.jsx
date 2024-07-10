868555ac6840b5e87454e3c0e4d5a55f05a59975
Alex Prostamo <alexprostamo@MacBook-Air-di-Alex.fritz.box>
2024-07-10 21:16:21 +0200

    other adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { Suspense } from 'react';
 import MenuItem from './MenuItem'
 import { AiFillHome } from 'react-icons/ai'
 import { BsFillInfoCircleFill } from 'react-icons/bs'
@@ -7,17 +7,19 @@ import DarkModeSwitch from './DarkModeSwitch'
 
 export default function Header() {
   return (
-    <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
-        <div className='flex gap-4'>
-            <MenuItem title="home" address="/" Icon={AiFillHome} />
-            <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
-        </div>
-        <div className='flex items-center gap-4'>
-            <DarkModeSwitch />
-            <Link href={"/"} className='flex gap-1 items-center'>
-                <span className='text-2xl font-bold text-[#000000]  bg-[#E0B518] py-1 px-2 rounded-lg'>IMDb</span>
-            </Link>
-        </div>
-    </div>
+    <Suspense>
+      <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
+          <div className='flex gap-4'>
+              <MenuItem title="home" address="/" Icon={AiFillHome} />
+              <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
+          </div>
+          <div className='flex items-center gap-4'>
+              <DarkModeSwitch />
+              <Link href={"/"} className='flex gap-1 items-center'>
+                  <span className='text-2xl font-bold text-[#000000]  bg-[#E0B518] py-1 px-2 rounded-lg'>IMDb</span>
+              </Link>
+          </div>
+      </div>
+    </Suspense>
   )
 }
