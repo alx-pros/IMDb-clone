79f2e2b1e3608998bf92cda5064bebf02b928c7f
Alex Prostamo <alexprostamo@MacBook-Air-di-Alex.fritz.box>
2024-07-10 21:25:46 +0200

    resolving bugs

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import Providers from "./Providers";
 import Navbar from "@/components/Navbar";
 import SearchBox from "@/components/SearchBox";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,12 +20,14 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/logo.png"/>
       </head>
       <body className={inter.className}>
-        <Providers>
-          <Header />
-          <Navbar />
-          <SearchBox />
-          {children}
-        </Providers>
+        <Suspense>
+          <Providers>
+            <Header />
+            <Navbar />
+            <SearchBox />
+            {children}
+          </Providers>
+        </Suspense>
       </body>
     </html>
   );
