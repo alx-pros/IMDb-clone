cf8713ebcb95733995961ef94b6b8f49b99ba9b7
Alex Prostamo <alexprostamo@MacBook-Air-di-Alex.fritz.box>
2024-07-10 18:33:42 +0200

    IMDb

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,31 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import Providers from "./Providers";
+import Navbar from "@/components/Navbar";
+import SearchBox from "@/components/SearchBox";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "IMDB clone",
+  title: "IMDb",
   description: "This is a movie database clone",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <link rel="icon" href="/logo.png"/>
+      </head>
+      <body className={inter.className}>
+        <Providers>
+          <Header />
+          <Navbar />
+          <SearchBox />
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
